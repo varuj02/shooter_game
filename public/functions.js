6cf664775abe10bb6980d22ac2967f07350facc4
varuj02 <cool.varuzh@gmil.com>
2018-06-12 18:33:52 +0400

    vs1.0.0

---
+++ public/functions.js
@@ -0,0 +1,73 @@
+
+// Detect the collision
+function Collision_right(coords) {
+    var obstacleX = coords.x;
+    var obstacleY = coords.y;
+
+    var playerOX = player.x + (side / 2);
+    var playerOY = player.y + (side / 2);
+
+    var objectOX = obstacleX + (side / 2);
+    var objectOY = obstacleY + (side / 2);
+
+    if (objectOX - playerOX <= side && objectOX - playerOX >= 0) {
+        if (Math.abs(playerOY - objectOY) < side) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function Collision_left(coords) {
+    var obstacleX = coords.x;
+    var obstacleY = coords.y;
+
+    var playerOX = player.x + (side / 2);
+    var playerOY = player.y + (side / 2);
+
+    var objectOX = obstacleX + (side / 2);
+    var objectOY = obstacleY + (side / 2);
+
+    if (playerOX - objectOX <= side && playerOX - objectOX >= 0) {
+        if (Math.abs(playerOY - objectOY) < side) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function Collision_up(coords) {
+    var obstacleX = coords.x;
+    var obstacleY = coords.y;
+
+    var playerOX = player.x + (side / 2);
+    var playerOY = player.y + (side / 2);
+
+    var objectOX = obstacleX + (side / 2);
+    var objectOY = obstacleY + (side / 2);
+
+    if (playerOY - objectOY <= side && playerOY - objectOY >= 0) {
+        if (Math.abs(playerOX - objectOX) < side) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function Collision_down(coords) {
+    var obstacleX = coords.x;
+    var obstacleY = coords.y;
+
+    var playerOX = player.x + (side / 2);
+    var playerOY = player.y + (side / 2);
+
+    var objectOX = obstacleX + (side / 2);
+    var objectOY = obstacleY + (side / 2);
+
+    if (objectOY - playerOY <= side && objectOY - playerOY >= 0) {
+        if (Math.abs(playerOX - objectOX) < side) {
+            return true;
+        }
+    }
+    return false;
+}
---
+++ public/script.js
@@ -0,0 +1,50 @@
+
+var canvasHeight = 10;
+var canvasWidth = 20 ;
+var side = 20;
+var player = {x:0,y:0,step:side/10}
+var obstacles =[{x:200,y:50},{x:100,y:100}]
+
+
+
+
+
+function setup(){
+    createCanvas(canvasWidth*side,canvasHeight*side);
+}
+function draw(){
+    background("#acacac");
+
+    fill("orange")
+    rect(player.x,player.y,side,side);
+
+    fill(0)
+    for(let obstacle of obstacles){
+        rect(obstacle.x,obstacle.y,side,side)
+    }
+
+    if(keyIsDown(RIGHT_ARROW)&&player.x+side<canvasWidth*side){
+        for (var coords of obstacles) {
+            if (Collision_right(coords)) return;
+        }
+        player.x+=player.step
+    }
+    if(keyIsDown(LEFT_ARROW)&&player.x>=0){
+        for (var coords of obstacles) {
+            if (Collision_left(coords)) return;
+        }
+        player.x-=player.step
+    }
+    if(keyIsDown(UP_ARROW)&&player.y>=0){
+        for (var coords of obstacles) {
+            if (Collision_up(coords)) return;
+        }
+        player.y-=player.step
+    }
+    if(keyIsDown(DOWN_ARROW)&&player.y+side<canvasHeight*side){
+        for (var coords of obstacles) {
+            if (Collision_down(coords)) return;
+        }
+        player.y+=player.step
+    }
+}
